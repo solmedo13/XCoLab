759921df3bb5ad6797348fdaba79061d7b1b36ac
Carlos Botelho <carlosbpf@gmail.com>
2017-03-21 14:39:31 -0300

    final wrapp for duplicate member task

--- view/src/main/java/org/xcolab/view/pages/profile/view/MergeDuplicateMemberController.java
+++ view/src/main/java/org/xcolab/view/pages/profile/view/MergeDuplicateMemberController.java
@@ -51,7 +51,48 @@ public class MergeDuplicateMemberController {
             Member originalMember = MembersClient.getMember(memberId);
             Member memberToBeJoined = MembersClient.getMember(memberToJoin);
 
-            //do the crazy things
+            /*
+            -- Before
+            -- user and groups and permissions
+            UPDATE User_ set userId = 2000811 where userId = 623;
+            UPDATE members_Member set id_ = 2000811 where id_ = 623;
+            UPDATE Users_Groups set userId = 2000811 where userId = 623;
+            UPDATE Users_Roles set userId = 2000811 where userId = 623;
+            UPDATE Contact_ set classPK = 2000811 , userId = 2000811 where classPK = 623 OR userId = 623;
+            UPDATE Group_ set creatorUserId = 2000811 where creatorUserId = 623;
+            UPDATE Group_ set classPK = 2000811 where classPK = 623;
+
+            UPDATE AssetEntry set userId = 2000811 , classPK = 2000811 where userId = 623 or classPK = 623;
+
+            -- activities
+            UPDATE xcolab_ActivitySubscription set receiverId = 2000811 where receiverId = 623;
+            UPDATE activities_ActivityEntry set memberId = 2000811 where memberId =623;
+
+            -- proposal
+            UPDATE xcolab_Proposal set authorId = 2000811 where authorId = 623;
+            UPDATE xcolab_ProposalMoveHistory set movingUserId = 2000811 where movingUserId = 623;
+            UPDATE xcolab_ProposalSupporter set userId = 2000811 where userId = 623;
+            UPDATE xcolab_ProposalVote set userId = 2000811 where userId = 623;
+
+            UPDATE Users_Groups set userId = 2000811 where userId = 623;
+
+            -- comment
+            UPDATE comment_Comment set authorId = 2000811 where authorId = 623;
+            UPDATE comment_Thread set authorId = 2000811 where authorId = 623;
+
+            -- messaging user pref
+            UPDATE xcolab_MessagingUserPreferences set userId = 2000811 where userId = 623;
+            UPDATE xcolab_Message set fromId = 2000811 where fromId = 623;
+            UPDATE xcolab_Message set repliesTo = 2000811 where repliesTo = 623;
+            UPDATE xcolab_MessageRecipientStatus set userId = 2000811 where userId = 623;
+
+            -- contest team member
+            UPDATE xcolab_ContestTeamMember set userId = 2000811 where userId = 623;
+
+
+            */
+
+
 
             AlertMessage.success("The profiles were merged, this is the original member's profile!").flash(request);
             response.sendRedirect("/members/profile/" + originalMember.getId_());
